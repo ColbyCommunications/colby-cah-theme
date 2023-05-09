2ca5b65716a729731840678ddca6db8968761ac3
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2023-05-09 10:24:19 -0400

    update http to https

--- resources/scripts/vue/HeaderMenuButton.vue
+++ resources/scripts/vue/HeaderMenuButton.vue
@@ -11,7 +11,7 @@
                         class="w-6 h-6 group-hover:scale-125 transform transition-transform"
                         aria-hidden="true"
                         viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
+                        xmlns="https://www.w3.org/2000/svg"
                     >
                         <path d="M 0 11 L 0 13 L 24 13 L 24 11 L 0 11 Z"></path>
                         <path d="M 0 18 L 0 20 L 24 20 L 24 18 L 0 18 Z"></path>
